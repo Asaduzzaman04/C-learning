7af45270a030e01b2c295b70b2023f8b51d5c640
Asaduzzaman04 <asadchy152@gmail.com>
2024-09-20 00:16:14 +0600

    class=1/2 pushed

---
+++ class_01/DataType.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+
+int main(){
+    int a = 1; // Create a intizer variable usng int variable keyword
+    float b = 3.1416; // Create a floating variable usng float variable keyword
+    char name = 'Asaduzzaman'; // Create a chracter variable using char keyword (char value  allows in single code)
+    
+}
---
+++ class_01/Variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int  main (){
+ int a; //variable declaration 
+ a = 6 ; //variable initialization
+
+}
+
+// {  int a b;
+//  a b = 8;
+//  this type of variable are not allows
+// variable name cannot have whitespace
+// variable name only allows latter using {cammelCase}
+//{
+// variave name are case sensetive ex => 
+//int a; int a; 
+//it will be count two different variable (try to make different name)
+//}
+//  }
